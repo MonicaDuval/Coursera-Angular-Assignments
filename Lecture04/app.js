6e9d55eb46f0534bf8affde18dd5293fe60bb3dd
Monique Dittrich <agentin007@onlinehome.de>
2020-07-13 12:22:33 +0200

    Update

--- Lecture04/app.js
+++ Lecture04/app.js
@@ -1,10 +1,15 @@
 (function () {
 'use strict';
-
-
+angular.
 angular.module('myFirstApp', [])
 
-  .controller('myFirstController', function () {
+// scope: shares data between the view and the viewmodel (angualr.js-spezifische funktion)
+
+  .controller('myFirstController', function ($scope) {
+    $scope.name = "Monica";
+    $scope.sayHello = function () {
+      return "Hello Babe"
+    }
 
   });
 
